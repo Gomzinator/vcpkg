2df2cce999e325a4113b5359408f1fa8f8f17a2c
Olivier GOMEZ <olivier.gomez2@cea.fr>
2019-10-24 11:24:56 +0200

    [g2o] update version to master (commit 2019-08-01) and patch this commit to add the feature "cholmod_solver" to correctly find the dependencies needed by g2o within vcpkg

--- ports/g2o/portfile.cmake
+++ ports/g2o/portfile.cmake
@@ -3,12 +3,18 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RainerKuemmerle/g2o
-    REF 20170730_git
-    SHA512 a85e3f79e6a8bd0f81a9a1a7a01227779100d9f4ebd0ae9c03537bbdcc246018f292b53045f027bbe28ecf63b98de2f22f5528c992c93c9790eb6a3a40995903
+    REF e7b5b7a1143bcbb6d57faab4bff8b2ab3ccd17a6
+    SHA512 e862669d5c87c84137a06623dd2bc041622c1451fec7ff37de2013b78987e21d7cfaf58fa0e2990c1df3ea73cb0a43a7c917eb7deab0f3e0185d4cd7e7bb6807
     HEAD_REF master
+    PATCHES
+      0001-option-cholmod-solver.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_LGPL_SHARED_LIBS)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    cholmod_solver G2O_USE_CHOLMOD
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -17,15 +23,14 @@ vcpkg_configure_cmake(
         -DBUILD_LGPL_SHARED_LIBS=${BUILD_LGPL_SHARED_LIBS}
         -DG2O_BUILD_EXAMPLES=OFF
         -DG2O_BUILD_APPS=OFF
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-if(VCPKG_USE_HEAD_VERSION)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/g2o)
-endif()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/g2o)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     file(GLOB_RECURSE HEADERS "${CURRENT_PACKAGES_DIR}/include/*")
